f66244e289691edbeaa84f8c99a7365dc08c46da
srishti <srishtiprasad500@gmail.com>
2022-02-05 17:48:16 -0800

    embeded ejs files

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ const { response } = require("express");
 const express=require("express");
 const http=require("https");
 const bodyParser=require("body-parser");
+const ejs = require("ejs");
+const { rawListeners } = require("process");
 const app=express()
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 require('dotenv').config();
-
+//getting error here
 app.get("/",function(req,res){
-   
-    res.sendFile(`${__dirname}/index.html`)
+    //res.sendFile(`${__dirname}/index.html`)
+    res.render("input");
     
 });
 app.post("/",function(req,res){
@@ -19,7 +22,7 @@ app.post("/",function(req,res){
    const unit="metric";
   
    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apikey+"&units=metric"
-http.get(url,function(response){
+    http.get(url,function(response){
     console.log(response.statusCode);
     response.on("data",function(data){
         const weatherData=JSON.parse(data);
@@ -27,10 +30,11 @@ http.get(url,function(response){
         const weatherDescription=weatherData.weather[0].description;
         const icon=weatherData.weather[0].icon;
         const imageURL=" http://openweathermap.org/img/wn/"+icon+"@2x.png";
-        res.write("<p>the weather  is currently"+weatherDescription+"</p>")
-        res.write("<h1> "+query+" temperature is "+temp+"</h1>");
-        res.write("<img src="+imageURL+ ">" )
-        res.send();
+        res.render("display",{query:query,temp:temp,weatherDescription:weatherDescription,imageURL:imageURL})
+        // res.write("<h1> "+query+" temperature is "+temp+"</h1>");
+        // res.write("<p>the weather  is currently"+weatherDescription+"</p>")
+        // res.write("<img src="+imageURL+ ">")
+        // res.send();
         // console.log(weatherData)
     })
 })
